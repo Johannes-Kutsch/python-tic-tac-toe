94f2bad92db8a4d0bd01c530808eed25a28f134c
Johannes Kutsch <johanneskutsch@live.de>
2025-12-04 16:18:24 +0100

    * some work on core gameloop

--- Game/GameManager.py
+++ Game/GameManager.py
@@ -11,14 +11,34 @@ class GameManager:
         self.board = Board()
 
     def start_game(self):
+        print("")
         print(repr(self.board))
 
-        # get row/column input for active player
-        # validate input format (if invalid display error + repeat last step)
+        while True:
+            player_input = self.get_valid_input()
+            if self.board.try_make_move(int(player_input[0])-1, int(player_input[-1])-1, self.playerManager.get_active_player_id()):
+                # player Feedback invalid position
+                break
+
+        print(repr(self.board))
         # try player move (if not possible display error + get new input)
 
         # switch player
         # repeat until win/draw
 
-    def get_input(self):
-        pass
+
+
+    def get_valid_input(self):
+        input_prompt = self.playerManager.get_active_player_name() + " to make a move, please enter cell and row (e.g. 1 2: "
+
+        while True:
+            string_input = input(input_prompt)
+            if self.is_input_valid(string_input):
+                return string_input
+
+            # player Feedback invalid input
+
+    @staticmethod
+    def is_input_valid(string_input):
+        valid_inputs = ["1", "2", "3"]
+        return len(string_input) > 1 and string_input[0] in valid_inputs and string_input[-1] in valid_inputs
